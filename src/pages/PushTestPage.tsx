c351d071003c6520bafa9e279fd067d56235fcdb
devLee98 <iiyoungchan@naver.com>
2026-04-22 15:21:23 +0900

    chore: baseURL 설정

--- src/pages/PushTestPage.tsx
+++ src/pages/PushTestPage.tsx
@@ -1,23 +1,30 @@
+import { onMessage } from "firebase/messaging";
+import { useEffect } from "react";
 import { requestPermissionAndGetToken } from "../lib/fcm";
+import { messaging } from "../lib/firebase";
 export default function PushTestPage() {
   const handleGetToken = async () => {
     const token = await requestPermissionAndGetToken();
     if (!token) return;
-    // 아직 백엔드 연동 안 됨
-    //   await fetch("/임시엔드포인트", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({ token }),
-    //   });
-    //   console.log("토큰 저장 요청 완료");
-    // };
-    // // useEffect(() => {
-    //   const unsubscribe = onMessage(messaging, (payload) => {
-    //     console.log("포그라운드 알림:", payload);
-    //   });
-    //   return () => unsubscribe();
-    // }, []);
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/fcm/token`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token }),
+      },
+    );
+    const data = await response.json();
+
+    console.log("받은 응답 =", data);
   };
+  useEffect(() => {
+    const unsubscribe = onMessage(messaging, (payload) => {
+      console.log("포그라운드 알림:", payload);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div style={{ padding: 24 }}>
       <h1>FCM 테스트 페이지</h1>
