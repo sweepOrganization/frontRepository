51dfb496eebe3473f332b7c671cace28b7223e8d
devLee98 <iiyoungchan@naver.com>
2026-04-23 13:19:43 +0900

    feat: 토큰 유무에 따라 /와 /login 라우팅 분기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import OAuthCallbackPage from "./pages/OAuthCallbackPage";
 import PushTestPage from "./pages/PushTestPage";
 
+function RootPage() {
+  const token = localStorage.getItem("accessToken");
+  if (!token) return <Navigate to="/login" replace />;
+  return <HomePage />;
+}
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<RootPage />} />
       <Route path="/push-test" element={<PushTestPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/oauth2/callback" element={<OAuthCallbackPage />} />
