d67ac031a9a2438e4af361b65fd9dbe04495ea78
devLee98 <iiyoungchan@naver.com>
2026-04-24 12:53:04 +0900

    구글 로그인

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,10 +3,15 @@ export default function LoginPage() {
     window.location.href =
       "https://sweepmap.duckdns.org/oauth2/authorization/kakao";
   };
+  const handleGoogleLogin = () => {
+    window.location.href =
+      "https://sweepmap.duckdns.org/oauth2/authorization/google";
+  };
   return (
     <div>
       <h1>LoginPage</h1>
       <button onClick={handleKakaoLogin}>카카오로그인</button>
+      <button onClick={handleGoogleLogin}>구글로그인</button>
     </div>
   );
 }
