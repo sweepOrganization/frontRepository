7e0d0be95de9d6be02d6af3597cf051207f6f4c9
devLee98 <iiyoungchan@naver.com>
2026-05-08 17:11:45 +0900

    chore:알람삭제테스트컴포넌트제거

--- src/pages/RoutePage.tsx
+++ src/pages/RoutePage.tsx
@@ -1,6 +1,5 @@
 ﻿import { useEffect, useMemo, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import DeleteModal from "../components/HomePage/DeleteModal";
 import useGetDetailAlarm from "../hooks/queries/useGetDetailAlarm";
 import useGetDetailRoute from "../hooks/queries/useGetDetailRoute";
 
@@ -698,7 +697,6 @@ export default function RoutePage() {
 
   return (
     <div className="flex h-screen flex-col overflow-hidden">
-      <DeleteModal />
       <div ref={mapRef} className="h-[285px] w-full" />
       <div className="mt-5 flex min-h-0 flex-1 flex-col px-4">
         <div className="shrink-0">
