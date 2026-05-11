06e11bd926601c435d4d5cdef4d5e2ff5875a532
devLee98 <iiyoungchan@naver.com>
2026-05-12 02:04:08 +0900

    feat: 스플래시 최소 3초 노출 후 앱 로딩 완료 시 화면 전환

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/common/Header";
 export default function Layout() {
   const { pathname } = useLocation();
   const hideHeader = pathname === "/login";
---
+++ src/components/common/Splash.tsx
@@ -0,0 +1,9 @@
+export default function Splash() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="splash-shine relative inline-block overflow-hidden">
+        <img src="/Splash.svg" alt="Splash" />
+      </div>
+    </div>
+  );
+}
